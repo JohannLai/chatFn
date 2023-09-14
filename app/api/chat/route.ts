1adabe3460f0287cc404ce6727de8b9fd62536c6
ZhiHang Li <johann_lai@outlook.com>
2023-09-14 23:22:34 +0800

    Update route.ts change to 3.5

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -92,7 +92,7 @@ export async function POST(req: Request) {
   const { messages, function_call } = await req.json()
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo-16k',
+    model: 'gpt-3.5-turbo',
     stream: true,
     messages,
     functions,
